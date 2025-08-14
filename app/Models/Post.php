6512cd08083de57abd20e8d5cec1298d08013c1d
Abdul azeez <azeez@buildnext.in>
2025-08-14 15:14:04 +0530

    feat:Part 1: Database Design and Migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable=[
+        'id',
+        'user_id',
+        'category_id',
+        'title',
+        'slug',
+        'content',
+        'status',
+        'published_at',
+    ];
 }
--- database/migrations/2025_08_14_092716_create_posts_table.php
+++ database/migrations/2025_08_14_092716_create_posts_table.php
@@ -13,9 +13,21 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('title');
+            $table->text('content');
+            $table->smallInteger('status');
+            $table->string('slug');
+            $table->dateTime('published_at')->nullable();
             $table->timestamps();
         });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
     }
+
 
     /**
      * Reverse the migrations.
